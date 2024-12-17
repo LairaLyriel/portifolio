f8a184a38e2501fdae1617ad67fa6ffeeaee37c1
Laira Lyriel <laira.lyriel2014@gmail.com>
2024-12-17 17:46:12 -0300

    finalização da section skills; section projetos; substituiçao das imagens por icons.

--- main/index.js
+++ main/index.js
@@ -1,8 +1,8 @@
 
 // BOTAO DOS CARD DE HAB
 
-const btnLogin = document.querySelector('#btnPessoais');
-const btnCadastrar = document.querySelector('#btnProfissionais');
+const btnPessoais = document.querySelector('#btnPessoais');
+const btnProfissionais = document.querySelector('#btnProfissionais');
 const pessoaisCard = document.querySelector('#pessoal');
 const profissionaisCard = document.querySelector('#profissional');
 const btnColor = document.querySelector('.btn-color');
@@ -11,11 +11,29 @@ btnPessoais.addEventListener('click', () => {
     pessoaisCard.classList.add('active');
     profissionaisCard.classList.remove('active');
     btnColor.style.left = "0";
-
 });
 
 btnProfissionais.addEventListener('click', () => {
     pessoaisCard.classList.remove('active');
     profissionaisCard.classList.add('active');
     btnColor.style.left = "50%";
+});
+
+// EFEITO DA DIGITAÇÃO
+document.addEventListener("DOMContentLoaded", () => {
+    const text = "< Olá! Eu sou Laira Lyriel />";
+    const typingElement = document.getElementById("typing-effect");
+    typingElement.textContent = "";
+
+    let index = 0;
+
+    function typeText() {
+        if (index < text.length) {
+            typingElement.textContent += text[index];
+            index++;
+            setTimeout(typeText, 150); //tempo
+        }
+    }
+
+    typeText();
 });
